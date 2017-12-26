c09c8ac6e9b592f62b5ff3f323ca5daa830d51f1
mikezander <mikezander87@yahoo.com>
2017-12-26 15:26:58 -0500

    Initial Commit

---
+++ image-to-voice-recognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  image-to-voice-recognition
+//
+//  Created by Michael Alexander on 12/26/17.
+//  Copyright © 2017 Michael Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
